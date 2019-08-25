c17ac038cbc0d0e8cdfd08c76ff5c847e3899c21
suyogdilipkale <suyog.kale@gmail.com>
2019-08-25 19:27:26 +0530

    added some comments and removed unwanted code

--- index.js
+++ index.js
@@ -9,31 +9,33 @@ const connection = mysql.createConnection({
 
 connection.connect();
 //start inserting new products
-insertProduct();
+//insertProduct();
 
 //start inserting new orders
 insertOrder();
 
 function insertOrder(){
   try{
-    //insert new customer
+    //step 1: insert new customer
     var customer  = {customername: faker.name.findName()};
     connection.query("INSERT INTO customer SET ?",customer, function (error, results, fields) {
         if (error) throw error;
         var customerId = results.insertId;
         console.log('The customer id is: ', customerId);
         //now insert some orders for this customer
-        //first get some random productIds
+        //step 2: first get some random productIds from database for this order
         var productIds = [];
         while(productIds.length < 8){
           var r = Math.floor(Math.random()*1000) + 1;
           if(productIds.indexOf(r) === -1) productIds.push(r);
         }
+        //step 3: create order master entry get orderid
         var order_master = {customerid:customerId};
         connection.query("INSERT INTO order_master SET ?",order_master, function (error, results, fields) {
             if (error) throw error;
             var orderId = results.insertId;
             console.log('The new order id is: ', results.insertId);
+            //step 4: against this order id enter multiple product purchase order entries in order_detail table
             for(i=1;i<productIds.length;i++){
               var order_detail = {orderid:orderId,productid:productIds[i],value:Math.floor(Math.random() * (+1000 - +10)) + +10};
               connection.query("INSERT INTO order_detail SET ?",order_detail, function (error, results, fields) {
@@ -44,8 +46,6 @@ function insertOrder(){
         });
 
     });
-
-
     //add more orders
     setTimeout(function(){ insertOrder();},1000);
   }
@@ -54,22 +54,7 @@ function insertOrder(){
   }
 }
 
-function insertCustomer(){
-  try{
-    //insert new customer
-    var customer  = {customername: faker.name.findName()};
-    connection.query("INSERT INTO customer SET ?",customer, function (error, results, fields) {
-        if (error) throw error;
-        var customerId = results.insertId;
-        console.log('The customer id is: ', customerId);
-        return customerId;
-    });
-  }
-  catch(e){
-    console.log("Exception in insertOrders:"+e);
-  }
-}
-
+//function to insert dummy products into the database
 function insertProduct(){
   try{
     //insert new product
@@ -79,7 +64,7 @@ function insertProduct(){
         var productId = results.insertId;
         console.log('The product id is: ', results.insertId);
     });
-    //add more orders
+    //add more products
     setTimeout(function(){ insertProduct();},300);
   }
   catch(e){
