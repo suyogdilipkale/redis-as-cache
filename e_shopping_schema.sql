c6c512df3c09281a97b668c83a44f733fbea043f
suyogdilipkale <suyog.kale@gmail.com>
2019-08-26 14:11:56 +0530

    added FK for the relevant tables

--- e_shopping_schema.sql
+++ e_shopping_schema.sql
@@ -2,26 +2,6 @@ DROP DATABASE IF EXISTS e_shopping;
 CREATE DATABASE e_shopping;
 USE e_shopping;
 
-CREATE TABLE order_master
-(
-  id         bigint(20) NOT NULL      AUTO_INCREMENT,
-  datetime   timestamp  NULL          DEFAULT CURRENT_TIMESTAMP,
-  customerid bigint(20) NOT NULL,
-
-  PRIMARY KEY (id)
-);
-
-CREATE TABLE order_detail
-(
-  id         bigint(20) NOT NULL      AUTO_INCREMENT,
-  datetime   timestamp  NULL          DEFAULT CURRENT_TIMESTAMP,
-  orderid    bigint(20) NOT Null,
-  productid  bigint(20) NOT NULL,
-  value      float                    DEFAULT NULL,
-
-  PRIMARY KEY (id)
-);
-
 CREATE TABLE product
 (
   id         bigint(20) NOT NULL      AUTO_INCREMENT,
@@ -37,4 +17,27 @@ CREATE TABLE customer
   customername   varchar(100)  DEFAULT NULL,
 
   PRIMARY KEY (id)
+);
+
+CREATE TABLE order_master
+(
+  id         bigint(20) NOT NULL      AUTO_INCREMENT,
+  datetime   timestamp  NULL          DEFAULT CURRENT_TIMESTAMP,
+  customerid bigint(20) NOT NULL,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (customerid) REFERENCES customer(id)
+);
+
+CREATE TABLE order_detail
+(
+  id         bigint(20) NOT NULL      AUTO_INCREMENT,
+  datetime   timestamp  NULL          DEFAULT CURRENT_TIMESTAMP,
+  orderid    bigint(20) NOT Null,
+  productid  bigint(20) NOT NULL,
+  value      float                    DEFAULT NULL,
+
+  PRIMARY KEY (id),
+  FOREIGN KEY (orderid) REFERENCES order_master(id),
+  FOREIGN KEY (productid) REFERENCES product(id)
 );
